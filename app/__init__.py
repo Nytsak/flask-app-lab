10fc7dc0a5b5a56d74eb1a9fc07e2d6d02629927
Oleh Nytsak <oleh.nytsak.23@pnu.edu.ua>
2025-10-22 19:23:10 +0300

    Add users blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_pyfile("../config.py")
 
-from . import routes
+from . import views
+from .users import users_bp
+
+app.register_blueprint(users_bp)
