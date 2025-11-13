bda95a34079206b3c94e1a96953e868bace51267
Oleh Nytsak <oleh.nytsak.23@pnu.edu.ua>
2025-11-13 21:57:45 +0200

    Add Post model and CategoryEnum for blog post management

---
+++ app/posts/models.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from app import db
+import enum
+
+
+class CategoryEnum(enum.Enum):
+    news = 'news'
+    publication = 'publication'
+    tech = 'tech'
+    other = 'other'
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    posted = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    category = db.Column(
+        db.Enum(CategoryEnum),
+        default=CategoryEnum.other,
+        nullable=False
+    )
+
+    def __repr__(self):
+        return f'<Post {self.id}: {self.title}>'
