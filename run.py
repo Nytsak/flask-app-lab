46cd5eb4d5006db7eea3b8479679dfb9c3348236
Oleh Nytsak <oleh.nytsak.23@pnu.edu.ua>
2025-11-13 23:53:53 +0200

    Add shell context for database and models in run.py

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
-from app import create_app
+import os
+from app import create_app, db
+from app.posts.models import Post
+from app.products.models import Product
 
-app = create_app(config_name="development")
+app = create_app(os.getenv('FLASK_ENV', 'development'))
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Post': Post, 'Product': Product}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
